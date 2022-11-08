bfe539b0bbf67792ff2134f5ae757831155b1539
emwong101 <emilywonglfa@gmail.com>
2022-11-07 20:29:54 -0800

    implemented use effect for switiching videos

--- src/App.js
+++ src/App.js
@@ -10,21 +10,38 @@ import VideoDescription from "./components/sections/videoDescription/VideoDescri
 
 //data
 import { getVideoDetails, getVideos } from "./utils/utils";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+const allVideos =
+  "https://project-2-api.herokuapp.com/videos?api_key=536ba9bc-8a92-4bd6-8a10-a3a876def07a";
+const specificVideo = (videoID) =>
+  `https://project-2-api.herokuapp.com/videos/${videoID}?api_key=536ba9bc-8a92-4bd6-8a10-a3a876def07a`;
 
 function App() {
   const [videoID, setVideoID] = useState(
     "84e96018-4022-434e-80bf-000ce4cd12b8"
   );
   const [video, setVideo] = useState(getVideos(videoID));
-  const [activeVideo, setActiveVideo] = useState(getVideoDetails(videoID));
-
+  const [activeVideo, setActiveVideo] = useState({});
   const handleClick = (event, videoID) => {
     event.preventDefault();
     setVideoID(videoID);
     setVideo(getVideos(videoID));
-    setActiveVideo(getVideoDetails(videoID));
+    setActiveVideo(specificVideo(videoID));
   };
+
+  useEffect(() => {
+    const render = async () => {
+      try {
+        const { data } = await axios.get(specificVideo(videoID));
+        setActiveVideo(data);
+      } catch (error) {
+        console.log("Error");
+      }
+    };
+    render();
+  }, [videoID]);
 
   return (
     <>
